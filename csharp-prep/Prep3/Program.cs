ea6ebc8b01e4ae64c6f78ba3f4651d7b4fa01398
Cigi <54045350+cigi801@users.noreply.github.com>
2023-01-08 16:48:59 -0700

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        int guess = 0;
+        int count = 0;
+
+        while (guess != number)
+        {
+        Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+            if (guess < number)
+            {
+                Console.WriteLine("Guess higher" );
+                count ++;
+            }
+            if (guess > number)
+            {
+                Console.WriteLine("Guess lower" );
+                count ++;
+            }
+        }
+
+        if(guess == number)
+        {
+            count ++;
+            Console.Write($"You guessed it! Guess count = {count} ");
+        }
     }
 }
