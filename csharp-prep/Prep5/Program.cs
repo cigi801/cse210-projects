d470e0e1ee819859dd729d8cf63bcda92f26b428
Cigi <54045350+cigi801@users.noreply.github.com>
2023-01-11 22:59:20 -0700

    Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        displayWelcome();
+
+        string userName = promptUserName();
+        int userNumber = promptUserNumber();
+        int squaredNumber = squareNumber(userNumber);
+        displayResult(userName, squaredNumber);
+    }
+
+    static void displayWelcome()
+    {
+        Console.WriteLine("Welcome to the program! ");
+    }
+
+    static string promptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int promptUserNumber()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int squareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void displayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square root of the number you entered is {square} ");
     }
 }
